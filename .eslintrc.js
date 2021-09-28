3597f98f3915c7e40a3b28ab4aa3bd698b649564
Stepan <kvantoreps@gmail.com>
2021-09-28 21:12:27 +0300

    chore: configure project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,41 @@
 module.exports = {
   root: true,
+
   env: {
-    node: true,
+    'browser': true,
+    'node': true
   },
+
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
+
   parserOptions: {
-    ecmaVersion: 2020,
+    'ecmaVersion': 12,
+    'sourceType': 'module'
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-plusplus': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'max-classes-per-file': 'warn',
+    '@typescript-eslint/no-var-requires': 'off',
+    'import/prefer-default-export': 'off',
+    'no-useless-constructor': 'off',
+    'lines-between-class-members': 'off',
+    'class-methods-use-this': 'off',
+    'arrow-parens': 'off'
+  },
+
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['~', 'src']]
+      },
+    },
   },
 };
