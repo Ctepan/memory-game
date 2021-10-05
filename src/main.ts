47e4b9fbe1e356e6114a388d261d4c9c2045d407
Stepan <kvantoreps@gmail.com>
2021-10-05 22:16:07 +0300

    feat(main): add hash history for gh-pages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import App from '@/App.vue';
 import store, { key } from '@/store';
 import Game from '@/features/game/Game.vue';
@@ -11,7 +11,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/memory-game/'),
+  history: createWebHashHistory('/memory-game/'),
   routes,
 });
 
